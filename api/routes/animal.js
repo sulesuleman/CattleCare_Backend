7eb93d12001f7c9dea4a31e7e901c7bb7e16e22a
Muhammad Suleman <mousuleman@gmail.com>
2021-11-30 01:08:48 +0500

    change in file

--- api/routes/animal.js
+++ api/routes/animal.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+
+
+
 const { createAnimals, getAllAnimals, getAllUserAnimals, getSpecificAnimalById, deleteAnimal, updateAnimal } = require("./../controllers/animal.controller");
 const { authMiddle } = require('../middleware/auth.middleware');
 const { send } = require('../middleware/uploadAttachment.middleware');
